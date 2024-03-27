a623771210f24744dd8b06b0e75ca7e65295d4ec
Bruna Chiarello <castilhos.chiarello@gmail.com>
2024-03-27 11:01:50 -0300

    ddd

--- CodigoEmCPP.cpp
+++ CodigoEmCPP.cpp
@@ -13,16 +13,23 @@ using namespace std;
 int sqrt_nr(int x, int i){
     if(i == 0) return 1;
 
-    return (sqrt_nr(x, i - 1) + (x/sqrt_nr(x,i-1)))/2;
+    int a = sqrt_nr(x, i - 1);
+
+    int b = x/sqrt_nr(x,i-1);
+
+    int c = a + b;
+
+    return c/2;
+
+    //return (sqrt_nr(x, i - 1) + (x/sqrt_nr(x,i-1)))/2;
 }
 
 int main(){
     int x, i, result;
+    
+    cout << "Programa de Raiz Quadrada - Newton-Rapson" << endl << "Desenvolvedores: < Sophia, Bruna >" << endl;
 
     while(1){
-        
-        cout << "Programa de Raiz Quadrada - Newton-Rapson" << endl << "Desenvolvedores: < Sophia, Bruna>" << endl;
-
         cout << "Digite os parâmetros de x e i para calcular sqrt_nr (x,i) ou -1 para abortar a execução" << endl;
 
         cin >> x >> i;
